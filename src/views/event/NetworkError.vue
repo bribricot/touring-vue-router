55a694641940de10f81cf985debcb8923e26e045
bribricot <kraemer.lucie@gmail.com>
2021-09-12 02:00:45 +0200

    Added ability to set flash messages

--- src/views/event/NetworkError.vue
+++ src/views/event/NetworkError.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="networkError">
     <h1>Uh-Oh!</h1>
-
     <h3>
       It looks like you're experiencing some network issues, please take a
       breath and <a href="#" @click="$router.go(-1)">click here</a> to try
--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -4,12 +4,18 @@
 </template>
 
 <script>
-
 export default {
   props: ['event'],
+  inject: ['GStore'], // Don't forget to inject here 
   methods: {
   	register() {
   	// Call to the API, if registered successfuly then redirect to Event Details, how do we trigger navigation programmatically? The push method helps navigate the user to this location.
+      this.GStore.flashMessage =
+        'You are successfully registered for ' + this.event.title
+
+      setTimeout(() => {
+        this.GStore.flashMessage = ''
+      }, 3000)
   	    this.$router.push({
         name: 'EventDetails',
         params: { id: this.event.id }
