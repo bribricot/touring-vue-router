2823ff50d004de7f503f2866dce53505b0b0dbd2
bribricot <kraemer.lucie@gmail.com>
2021-09-14 00:00:04 +0200

    Unsaved change in Edit component

--- src/views/event/Edit.vue
+++ src/views/event/Edit.vue
@@ -5,7 +5,27 @@
 <script>
 
 export default {
-  props: ['event']
+  props: ['event'],
+  data: function() {
+    return {
+      unsavedChanges: true
+    }
+  },
+  beforeRouteLeave(routeTo, routeFrom, next) {
+    if (this.unsavedChanges) {
+      const answer = window.confirm(
+        'Do you really want to leave? You have unsaved changes!'
+      )
+      if (answer) {
+        next() // <-- Confirms the navigation
+      } else {
+        next(false) // <-- Cancels the navigation
+      }
+    } else {
+      next() // <-- Confirms the navigation
+    }
+  }
+
 }
 
 </script>
