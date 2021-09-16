edffae06957008e28ac2f004da10a9168c79dd58
bribricot <kraemer.lucie@gmail.com>
2021-09-16 16:59:01 +0200

    Global and Per-Route Guards, Progress Bar & Pagination fixes

--- src/router/index.js
+++ src/router/index.js
@@ -7,7 +7,9 @@ import EventEdit from '../views/event/Edit.vue'
 import About from '../views/About.vue'
 import NotFound from '@/views/event/NotFound.vue'
 import NetworkError from '@/views/event/NetworkError.vue'
-
+import NProgress from 'nprogress'
+import EventService from '@/services/EventService.js' // Remove from Layout.vue for Per-Route Guards
+import GStore from '@/store'
 
 const routes = [
   {
@@ -21,6 +23,24 @@ const routes = [
     name: 'EventLayout',
     props: true,
     component: EventLayout,
+    // Per-Route Guards, API call is here now. Be careful, we have no access to this anymore. Also add a return for the progress bar to wait until the API call is made. 
+    beforeEnter: to => {
+      return EventService.getEvent(to.params.id) // We have access to the target route, return and params.id
+      .then(response => {
+       GStore.event = response.data // (It's time for us to use the global store to store the event from our API call.
+      })
+      .catch(error => {
+        if (error.response && error.response.status == 404) {
+         //this.$router.push({}) Just return the path we want to redirect to. Same thing for 'else'.
+         	return {
+            name: '404Resource',
+            params: { resource: 'event' }
+          }
+        } else {
+          return { name: 'NetworkError' }
+        }
+      })
+    },
     children: [
       {
         path: '',
@@ -77,4 +97,13 @@ const router = createRouter({
   routes
 })
 
+// For having a global progress bar
+
+router.beforeEach(() => {
+  NProgress.start()
+})
+
+router.afterEach(() => {
+  NProgress.done()
+})
 export default router
--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,6 @@
-import { createStore } from 'vuex'
+// Create a reactive object allows us to store a global state instead of using VueX for now.
 
-export default createStore({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
-})
+import { reactive } from 'vue' // <--- importing reactive
+
+export default reactive({ flashMessage: '', event: null })
+// Add also the event property, because it's where we'll store the data returned from our API, and where our component will look for the data.
